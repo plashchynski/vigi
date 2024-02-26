c2d30a73332d4e5d49e5a58e379b79900910599d
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-02-27 01:13:24 +0300

    Add a simple PubSub implementation for video streaming

---
+++ agent/vigi_agent/pub_sub.py
@@ -0,0 +1,37 @@
+from queue import Queue
+import threading
+
+class PubSub:
+    """
+    A simple publish-subscribe (PubSub) class that allows multiple subscribers to
+    receive messages from a single publisher on a single topic.
+    """
+    def __init__(self):
+        self.subscribers = []
+        # Lock to prevent race conditions when accessing the list of
+        # subscribers from different threads
+        self.lock = threading.Lock()
+
+    def subscribe(self):
+        """
+        Subscribe to the PubSub object and return a queue to receive messages.
+        """
+        queue = Queue()
+        with self.lock:
+            self.subscribers.append(queue)
+        return queue
+
+    def unsubscribe(self, subscriber_queue):
+        """
+        Unsubscribe from the PubSub object.
+        """
+        with self.lock:
+            self.subscribers.remove(subscriber_queue)
+
+    def publish(self, message):
+        """
+        Publish a message to all the subscribers.
+        """
+        with self.lock:
+            for subscriber in self.subscribers:
+                subscriber.put(message)
