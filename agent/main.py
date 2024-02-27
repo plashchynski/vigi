35f6e2d2ff063624711348967a8dfa0450e13595
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-02-27 13:45:03 +0300

    Enable logging

--- agent/main.py
+++ agent/main.py
@@ -1,18 +1,28 @@
+import sys
+import logging
 import configparser
+
+logging.basicConfig(level=logging.INFO)
 
 from vigi_agent.app import app
 from vigi_agent.camera_monitor import CameraMonitor
 
-# Read the configuration file
-print("Reading the configuration file... ", end="", flush=True)
+logging.info("Reading the configuration file... ")
 config = configparser.ConfigParser()
 config.read('vigi.ini')
 app.user_config = config['DEFAULT']
-print("done!", flush=True)
+logging.info("Configuration file read successfully.")
 
+if app.user_config['Debug'] == 'True':
+    logging.getLogger().setLevel(logging.DEBUG)
+
+logging.debug(f"Configuration: {dict(app.user_config)}")
+
+logging.info("Starting the camera monitor... ")
 camera_monitor = CameraMonitor(camera_id=int(app.user_config['CameraID']))
 camera_monitor.start()
 app.camera_monitor = camera_monitor
+logging.info("Camera monitor started successfully.")
 
-# run Flask app
+logging.info("Starting the Flask web server... ")
 app.run(host='127.0.0.1', port=5000)
