0fadb2a05235e880c232d1d6e56e29098eee7275
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-03-02 00:09:49 +0300

    Add email notification provider

---
+++ agent/tests/notification_providers/test_email_notification_provider.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest.mock import patch
+
+from vigi_agent.notification_providers.email_notification_provider import EmailNotificationProvider
+
+# # to have a wild test, please provide your own email credentials
+# # and uncomment the following lines:
+# class TestEmailNotificationProviderWild(unittest.TestCase):
+#     def test_notify(self):
+#         # notify should send an email to each recipient
+#         provider = EmailNotificationProvider(
+#             smtp_server = "smtp.gmail.com",
+#             smtp_port = 587,
+#             smtp_user = "...",
+#             smtp_password = "...",
+#             sender_email = "...",
+#             recipient_emails = ["..."]
+#         )
+#         provider.notify("Test notification")
+
+class TestEmailNotificationProvider(unittest.TestCase):
+     @patch('vigi_agent.notification_providers.email_notification_provider.smtplib.SMTP')
+     def test_notify(self, mock_smtp):
+        provider = EmailNotificationProvider(
+            smtp_server = "smtp.gmail.com",
+            smtp_port = 587,
+            smtp_user = "user",
+            smtp_password = "password",
+            sender_email = "sender@example.com",
+            recipient_emails = ["recipient1@example.com",
+                                "recipient2@example.com"]
+        )
+        provider.notify("Test notification")
+        self.assertEqual(mock_smtp.call_count, 2)  # Verify SMTP was called twice (once for each recipient)
---
+++ agent/vigi_agent/notification_providers/email_notification_provider.py
@@ -0,0 +1,47 @@
+# This file contains the EmailNotificationProvider class which is used to send email notifications
+import logging
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+class EmailNotificationProvider:
+    def __init__(self, smtp_server: str, smtp_port: int, smtp_user: str, smtp_password: str, sender_email: str,
+                 recipient_emails: list[str]):
+        """
+        :param smtp_server: SMTP server address
+        :param smtp_port: SMTP server port
+        :param smtp_user: SMTP server user
+        :param smtp_password: SMTP server password
+        :param sender_email: Email address of the sender
+        :param recipient_emails: List of email addresses of the recipients
+        """
+        self.smtp_server = smtp_server
+        self.smtp_port = smtp_port
+        self.smtp_user = smtp_user
+        self.smtp_password = smtp_password
+        self.sender_email = sender_email
+        self.recipient_emails = recipient_emails
+
+    def notify(self, notification_text: str):
+        """
+        Send email notification to the recipients
+        """
+        for recipient_email in self.recipient_emails:
+            self._send_email(notification_text, recipient_email)
+
+    def _send_email(self, notification_text: str, recipient_email: str):
+        """
+        Send email to a single recipient
+        """
+        logging.info(f"Sending email from {self.sender_email} to {recipient_email} with text: {notification_text}")
+
+        message = MIMEMultipart()
+        message["From"] = self.sender_email
+        message["To"] = recipient_email
+        message["Subject"] = "ViGi Agent Notification"
+        message.attach(MIMEText(notification_text, 'plain'))
+
+        with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+            server.login(self.smtp_user, self.smtp_password)
+            server.sendmail(self.sender_email, recipient_email, message.as_string())
