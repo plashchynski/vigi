d4be484738f8afb026cdb9644496ee9d3b58edd7
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-03-01 12:54:09 +0300

    Add unit tests for context processors

---
+++ agent/tests/test_context_processors.py
@@ -0,0 +1,23 @@
+import unittest
+
+from vigi_agent.context_processors import utility_processor
+
+class TestUtilityProcessor(unittest.TestCase):
+    def setUp(self):
+        self.utilities = utility_processor()
+
+    def test_format_time(self):
+        # Test that the format_time function returns the expected result as a time string
+        self.assertEqual(self.utilities['format_time']("12-34-56"), "12:34:56")
+
+    def test_format_duration_none(self):
+        # Test the format_duration function should return "N/A" if the duration is None
+        self.assertEqual(self.utilities['format_duration'](None), "N/A")
+
+    def test_format_duration_seconds(self):
+        # Test formatting of duration in seconds
+        self.assertEqual(self.utilities['format_duration'](1), "1 second")
+        self.assertEqual(self.utilities['format_duration'](2), "2 seconds")
+
+        # Test formatting of duration in minutes (60 seconds)
+        self.assertEqual(self.utilities['format_duration'](60), "1 minute")
--- agent/vigi_agent/context_processors.py
+++ agent/vigi_agent/context_processors.py
@@ -1,3 +1,6 @@
+# This file contains the context processors for views
+# More info: https://flask.palletsprojects.com/en/3.0.x/templating/#context-processors
+
 import datetime
 import humanize
 
@@ -9,8 +12,9 @@ def utility_processor():
     def format_duration(duration):
         if duration is None:
             return "N/A"
-        # format duration as HH:MM:SS
-        return humanize.naturaldelta(datetime.timedelta(seconds=duration))
+
+        # format duration as n seconds, n minutes, n hours, n days, etc.
+        return humanize.precisedelta(datetime.timedelta(seconds=duration))
 
     return dict(
             format_time=format_time,
